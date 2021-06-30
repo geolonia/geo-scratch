c8699cbb4bebdfe45ec16b31ab39766f14f57e1b
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2021-06-30 19:41:55 +0900

    removed unused command

--- bin/rsync.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-rsync -avz extensions/geolonia/gui/ src/lib/libraries/extensions/
-rsync -avz extensions/geolonia/vm/ node_modules/scratch-vm/src/extension-support/
