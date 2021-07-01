cd577dec0e0871a8d9b0b6933dff098c4fad1dc0
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2021-07-01 16:32:38 +0900

    some fix

--- extensions/geolonia/vm/extension-manager.js
+++ extensions/geolonia/vm/extension-manager.js
@@ -13,6 +13,17 @@ const builtinExtensions = {
     // but serves as a reference for loading core blocks as extensions.
     coreExample: () => require('../blocks/scratch3_core_example'),
     // These are the non-core built-in extensions.
+    pen: () => require('../extensions/scratch3_pen'),
+    wedo2: () => require('../extensions/scratch3_wedo2'),
+    music: () => require('../extensions/scratch3_music'),
+    microbit: () => require('../extensions/scratch3_microbit'),
+    text2speech: () => require('../extensions/scratch3_text2speech'),
+    translate: () => require('../extensions/scratch3_translate'),
+    videoSensing: () => require('../extensions/scratch3_video_sensing'),
+    ev3: () => require('../extensions/scratch3_ev3'),
+    makeymakey: () => require('../extensions/scratch3_makeymakey'),
+    boost: () => require('../extensions/scratch3_boost'),
+    gdxfor: () => require('../extensions/scratch3_gdx_for'),
     geolonia: () => require('./geolonia/index'),
 };
 
