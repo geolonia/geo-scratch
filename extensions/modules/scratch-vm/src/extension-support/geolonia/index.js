28f2850804c02c0bcac3f872dc3bca07e85d6e79
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2021-07-03 02:36:39 +0900

    some fix

--- extensions/modules/scratch-vm/src/extension-support/geolonia/index.js
+++ extensions/modules/scratch-vm/src/extension-support/geolonia/index.js
@@ -2,6 +2,7 @@ const ArgumentType = require('../../extension-support/argument-type');
 const BlockType = require('../../extension-support/block-type');
 const Cast = require('../../util/cast');
 const formatMessage = require('format-message');
+const { resolve } = require('core-js/fn/promise');
 
 const Message = {
 }
@@ -63,24 +64,38 @@ class Scratch3GeoloniaBlocks {
                 {
                     opcode: 'displayMap',
                     blockType: BlockType.COMMAND,
-                    text: '地図を表示',
+                    text: '地図を経度 [LNG] 緯度 [LAT] ズーム [ZOOM] で表示',
+                    arguments: {
+                        LNG: {
+                          type: ArgumentType.NUMBER,
+                          defaultValue: 0,
+                        },
+                        LAT: {
+                          type: ArgumentType.NUMBER,
+                          defaultValue: 0,
+                        },
+                        ZOOM: {
+                          type: ArgumentType.NUMBER,
+                          defaultValue: 10,
+                        },
+                      }
                 },
                 {
                     opcode: 'flyTo',
                     blockType: BlockType.COMMAND,
-                    text: "経度 [LNG] 緯度 [LAT] ズーム [ZOOM] に移動",
+                    text: "経度 [LNG] 緯度 [LAT] ズーム [ZOOM] にジャンプ",
                     arguments: {
                       LNG: {
                         type: ArgumentType.NUMBER,
-                        defaultValue: 139.74
+                        defaultValue: 139.74,
                       },
                       LAT: {
                         type: ArgumentType.NUMBER,
-                        defaultValue: 35.65
+                        defaultValue: 35.65,
                       },
                       ZOOM: {
                         type: ArgumentType.NUMBER,
-                        defaultValue: 10
+                        defaultValue: 10,
                       },
                     }
                 },
@@ -91,27 +106,33 @@ class Scratch3GeoloniaBlocks {
     }
 
     displayMap(args) {
-        const mapContainer = document.getElementById('geolonia')
+        const promise = new Promise((resolve) => {
+            const mapContainer = document.getElementById('geolonia')
 
-        if (document.getElementById('map')) {
-            mapContainer.removeChild(document.getElementById('map'))
-        }
+            if (document.getElementById('map')) {
+                mapContainer.removeChild(document.getElementById('map'))
+            }
 
-        div = document.createElement("div");
-        div.id = 'map';
-        div.setAttribute("style", "width:100%;height:100%;");
-        div.dataset.navigationControl = 'off';
-        div.dataset.pitch = 40;
+            div = document.createElement("div");
+            div.id = 'map';
+            div.setAttribute("style", "width:100%;height:100%;");
+            div.dataset.navigationControl = 'off';
 
-        mapContainer.appendChild(div);
+            mapContainer.appendChild(div);
 
-        this.map = {}
-        this.map = new geolonia.Map({
-            container: '#map',
-            style: 'geolonia/gsi',
-            center: [0, 0],
-            zoom: 0,
-        });
+            this.map = {}
+            this.map = new geolonia.Map({
+                container: '#map',
+                style: 'geolonia/gsi',
+                center: [args.LNG, args.LAT],
+                zoom: args.LAT,
+                pitch: 40,
+            });
+
+            this.map.once('load', () => {
+                resolve()
+            })
+        })
     }
 
     flyTo(args) {
